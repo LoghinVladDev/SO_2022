60fb845f6894a9f8c715606de97a501008d7b06c
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2022-05-30 00:06:07 +0300

    Exemplu de utilizare FIFO

--- so_19_5/cmake-build-debug/cmake_install.cmake
+++ so_19_5/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/stefan/Documents/GitHub/Projects/so_19_5
+# Install script for directory: /home/stefan/Documents/GitHub/LaboratoareSo/SO_2022/so_19_5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/stefan/Documents/GitHub/Projects/so_19_5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/stefan/Documents/GitHub/LaboratoareSo/SO_2022/so_19_5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
